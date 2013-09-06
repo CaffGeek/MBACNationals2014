35f772502f1f7ad58746b9b74594784aa6d2e195
Chad Hurd <gibble+github@gmail.com>
2013-09-05 22:19:22 -0500

    Added a basic theme for bootstrap

--- WebFrontend/App_Start/BootstrapBundleConfig.cs
+++ WebFrontend/App_Start/BootstrapBundleConfig.cs
@@ -8,9 +8,9 @@ namespace WebFrontend.App_Start
 	{
 		public static void RegisterBundles()
 		{
-			BundleTable.Bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include("~/Scripts/bootstrap.js"));
-			BundleTable.Bundles.Add(new StyleBundle("~/Content/bootstrap/base").Include("~/Content/bootstrap/bootstrap.css"));
-			BundleTable.Bundles.Add(new StyleBundle("~/Content/bootstrap/theme").Include("~/Content/bootstrap/bootstrap-theme.css"));
+            BundleTable.Bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include("~/Scripts/bootstrap.js"));
+            BundleTable.Bundles.Add(new StyleBundle("~/Content/bootstrap/base").Include("~/Content/bootstrap/bootstrap.css"));
+            BundleTable.Bundles.Add(new StyleBundle("~/Content/bootstrap/theme").Include("~/Content/bootstrap/themes/justified-nav.css"));
 		}
 	}
 }
