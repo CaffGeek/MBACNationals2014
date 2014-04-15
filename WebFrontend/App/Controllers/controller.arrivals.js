6323e764d777c62d5746a7733e4d7bead9e01564
Chad Hurd <gibble+github@gmail.com>
2014-04-15 00:06:26 -0500

    Work on Arrivals/Departures UI

--- WebFrontend/App/Controllers/controller.arrivals.js
+++ WebFrontend/App/Controllers/controller.arrivals.js
@@ -6,9 +6,12 @@
         var lastSlash = url.lastIndexOf('/');
         var province = url.slice(lastSlash+1);
 
+        var emptyArrival = { ArriveAt: '2014-06-28T09:00' };
+        var emptyDeparture = { DepartAt: '2014-07-03T09:00' };
+
         $scope.model = {
-            arrivals: [{}],
-            departures: [{}]
+            arrivals: [angular.copy(emptyArrival), angular.copy(emptyArrival), angular.copy(emptyArrival)],
+            departures: [angular.copy(emptyDeparture), angular.copy(emptyDeparture), angular.copy(emptyDeparture)]
         };
 
         if (province) {
@@ -17,6 +20,22 @@
             //        $scope.model.participants = participants;
             //    });
         }
+
+        $scope.addArrival = function () {
+            $scope.model.arrivals.push(angular.copy(emptyArrival));
+        };
+
+        $scope.addDeparture = function () {
+            $scope.model.departures.push(angular.copy(emptyDeparture));
+        };
+
+        $scope.saveTravelInfo = function () {
+            alert('TODO:');
+        };
+
+        $scope.removeRecord = function () {
+            alert('TODO:');
+        };
     };
 
     app.controller("ArrivalsController", ["$scope", "$http", "$q", "$location", "modalFactory", "dataService", arrivalsController]);
