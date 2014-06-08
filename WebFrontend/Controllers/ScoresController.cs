9217fb73ade9d37a90e9fb6fa9976bf18515f3cb
Chad Hurd <gibble+github@gmail.com>
2014-06-08 16:11:18 -0500

    allow origin

--- WebFrontend/Controllers/ScoresController.cs
+++ WebFrontend/Controllers/ScoresController.cs
@@ -18,6 +18,8 @@ namespace WebFrontend.Controllers
         [OutputCache(NoStore = true, Duration = 0, VaryByParam = "None")]
         public JsonResult Schedule(string division)
         {
+            Response.AppendHeader("Access-Control-Allow-Origin", "*");
+
             var schedule = Domain.ScheduleQueries.GetSchedule(division);
             return Json(schedule, JsonRequestBehavior.AllowGet);
         }
